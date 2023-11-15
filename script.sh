b54d9b2398fbca9b1114a438a941fd3da0cd9fbb
Daniel Flores <daniel.floresvargas666@gmail.com>
2023-11-15 12:26:32 -0400

    add: script

---
+++ script.sh
@@ -0,0 +1,23 @@
+!#/bin/bash
+
+# instalar yay
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si
+
+# volver al home
+cd ~
+
+# intalar lo necesario
+yay -S bspwm rofi nitrogen polybar kitty dunst \ 
+  ttf-iosevka-nerd picom-simpleanims-git 
+
+# clonamos el repositorio
+git clone https://github.com/xOnlinE/bspwm
+
+# copiamos las configuraciones
+cp -rf ./bspwm/bspwm ~/.config
+cp -rf ./bspwm/sxhkd ~/.config
+cp -rf ./bspwm/rofi ~/.config
+cp -rf ./bspwm/dunst ~/.config
+cp -rf ./bspwm/picom ~/.config
